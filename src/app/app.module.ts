583db748d3a257d901a507f7d9e1febf6fc522f4
Tibi <bukkfabiszti@gmail.com>
2019-01-30 09:30:58 +0100

    generated car-list and car-form component with the CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarListComponent } from './car-list/car-list.component';
+import { CarFormComponent } from './car-form/car-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarListComponent,
+    CarFormComponent
   ],
   imports: [
     BrowserModule,
